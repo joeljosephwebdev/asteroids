9ec81136eddb4a7f4e3562d4c536aac95816c67d
joeljosephwebdev <joelrajoseph@outlook.com>
2024-09-11 01:27:32 -0700

    Fixed a bug with the player rotate method that prevented the player from turning continuously. and another with the update method that had the turn directions incorrect.

--- player.py
+++ player.py
@@ -19,13 +19,12 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
-        self.rotation = PLAYER_TURN_SPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(dt)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
-            inverse = dt * -1
-            self.rotate(inverse)
+            self.rotate(dt)
