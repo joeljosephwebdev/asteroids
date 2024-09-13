18019cabd282df88c5a3100bad001c3bb4e5cd25
joeljosephwebdev <joelrajoseph@outlook.com>
2024-09-13 15:12:07 -0700

    created asteroid class. created asteroid field class to spawn asteroids. used pygame groups to cleanup game loop in main.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidField import AsteroidField
 
 def main():
     pygame.init()
@@ -9,17 +11,32 @@ def main():
     
     game_clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+
     
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidField = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for item in updatable:
+            item.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 fps
